a6cbd44aef15c5222bfe4516fef87aa2a98b946f
Abdualalem <abdu20162020@gmail.com>
2019-04-07 22:44:55 +0200

    AddAndDelete

---
+++ sprint2/quiz/src/main/java/sw2project/Controller/Entities/Repository/QuistionEntitiesRepository.java
@@ -0,0 +1,12 @@
+package sw2project.Controller.Entities.Repository;
+
+import java.awt.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import sw2project.Controller.Entities.Quiz;
+import sw2project.Controller.Entities.quistion;
+
+public interface QuistionEntitiesRepository extends CrudRepository<quistion,Integer> {
+	
+}
